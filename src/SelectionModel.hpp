1279b0a78a51fb5ba0c3d9fef84dd92cdc0e68f5
Chukwudi <nwutobo@outlook.com>
2024-07-27 13:30:48 +0100

    🚀 Migrate from TdLib JSON to TdLib C++ interface 🚀
    - 🔄 Replace JSON-based API calls with C++ interface methods
    - 💻 Update relevant parts of the code to use TdLib's C++ API
    - ⚡ Improve performance and maintainability by leveraging C++ features

--- src/SelectionModel.hpp
+++ src/SelectionModel.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <td/telegram/td_api.h>
+
 #include <QAbstractListModel>
+
+#include <vector>
 
 class Locale;
 
@@ -8,7 +12,6 @@ class CountryModel : public QAbstractListModel
 {
     Q_OBJECT
 
-    Q_PROPERTY(QVariantList countries READ countries WRITE setCountries NOTIFY countriesChanged)
     Q_PROPERTY(int count READ count NOTIFY countChanged)
     Q_PROPERTY(int defaultIndex READ getDefaultIndex NOTIFY countChanged)
 
@@ -16,12 +19,12 @@ public:
     CountryModel(QObject *parent = nullptr);
 
     enum CountryRoles {
-        Iso2Role = Qt::UserRole + 1,
+        NameRole = Qt::UserRole + 1,
+        Iso2Role,
         CodeRole,
     };
 
-    const QVariantList &countries() const noexcept;
-    void setCountries(QVariantList countries);
+    void setCountries(td::td_api::object_ptr<td::td_api::countries> &&value);
 
     int rowCount(const QModelIndex &index = QModelIndex()) const override;
 
@@ -32,22 +35,18 @@ public:
     int count() const noexcept;
 
 signals:
-    void countriesChanged();
     void countChanged();
 
 private:
     int getDefaultIndex() const noexcept;
 
-    QVariantList m_countries;
+    std::vector<td::td_api::object_ptr<td::td_api::countryInfo>> m_countries;
 };
 
 class ChatFolderModel : public QAbstractListModel
 {
     Q_OBJECT
 
-    Q_PROPERTY(QObject *locale READ locale WRITE setLocale)
-
-    Q_PROPERTY(QVariantList chatFolders READ getChatFolders WRITE setChatFolders NOTIFY chatFoldersChanged)
     Q_PROPERTY(int count READ count NOTIFY countChanged)
 
 public:
@@ -55,65 +54,27 @@ public:
 
     enum ChatFolderRole {
         IdRole = Qt::UserRole + 1,
+        TitleRole,
         IconNameRole,
     };
 
-    QObject *locale() const;
-    void setLocale(QObject *locale);
+    void setLocale(Locale *locale);
 
-    QVariantList getChatFolders() const noexcept;
-    void setChatFolders(QVariantList value) noexcept;
+    void setChatFolders(std::vector<td::td_api::object_ptr<td::td_api::chatFolderInfo>> &&value) noexcept;
 
     int rowCount(const QModelIndex &index = QModelIndex()) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
 
-    Q_INVOKABLE QVariantMap get(int index) const noexcept;
+    Q_INVOKABLE QVariant get(int index) const noexcept;
 
     int count() const noexcept;
 
 signals:
     void countChanged();
-    void chatFoldersChanged();
 
 private:
     Locale *m_locale;
 
-    QVariantList m_chatFolders;
-};
-
-class FlexibleListModel : public QAbstractListModel
-{
-    Q_OBJECT
-
-    Q_PROPERTY(int count READ count NOTIFY countChanged)
-    Q_PROPERTY(QVariantList values READ values WRITE setValues NOTIFY valuesChanged)
-
-public:
-    explicit FlexibleListModel(QObject *parent = nullptr);
-
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-
-    int count() const noexcept;
-
-    QVariantList values() const noexcept;
-    void setValues(const QVariantList &values) noexcept;
-
-    Q_INVOKABLE void append(const QVariant &value) noexcept;
-    Q_INVOKABLE void remove(int index) noexcept;
-    Q_INVOKABLE QVariant get(int index) const noexcept;
-    Q_INVOKABLE void clear() noexcept;
-    Q_INVOKABLE void replace(const QString &key, const QVariant &value) noexcept;
-    Q_INVOKABLE void insert(int index, const QVariant &value) noexcept;
-
-    Q_INVOKABLE void sort(const QString &criteria, bool ascending);
-
-signals:
-    void countChanged();
-    void valuesChanged();
-
-private:
-    QVariantList m_values;
-    QHash<int, QByteArray> m_roleNames;
+    std::vector<td::td_api::object_ptr<td::td_api::chatFolderInfo>> m_chatFolders;
 };
